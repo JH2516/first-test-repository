892d9246135fca0106af354eb025b3f110c97a02
JH2516 <kaizer2516@gmail.com>
2023-03-27 15:45:09 +0900

    week4 updated

---
+++ Lecture04/a.cpp
@@ -0,0 +1,6 @@
+#include  <iostream>
+int main()
+{
+	std::cout<<"나는 머리가 아픈 사람이다"<<std::endl;
+	return 0;
+}
---
+++ Lecture04/star.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+	while(1)
+	{
+		if(_kbhit())
+		{
+			if(getch() == 27) break;
+		}
+		std::cout<<"*";
+	}
+	return 0;
+}
